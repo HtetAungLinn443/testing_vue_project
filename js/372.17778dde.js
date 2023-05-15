b63dcaf15bdf83fd6035c37f3f5cacb918f75d75
Htet Aung Linn <htetaunglinn443@gmail.com>
2023-05-16 03:36:34 +0630

    first commit

---
+++ js/372.17778dde.js
@@ -0,0 +1,21 @@
+"use strict";
+(self["webpackChunkblog_app"] = self["webpackChunkblog_app"] || []).push([
+    [372], {
+        372: function(e, n, t) {
+            t.r(n), t.d(n, { default: function() { return f } });
+            var r = t(252);
+            const a = { class: "d-flex justify-content-center align-items-center", style: { height: "100vh" } },
+                s = (0, r._)("h1", { class: "text-danger" }, "404 Error", -1),
+                c = [s];
+
+            function u(e, n, t, s, u, i) { return (0, r.wg)(), (0, r.iD)("div", a, c) }
+            var i = { name: "404_Page" },
+                l = t(744);
+            const o = (0, l.Z)(i, [
+                ["render", u]
+            ]);
+            var f = o
+        }
+    }
+]);
+//# sourceMappingURL=372.17778dde.js.map
